373835cf2dff94c25481f7382925f02728a480ae
GalladeFan475 <smashingsanjay6@gmail.com>
2025-03-17 10:54:43 +0800

    all buttons except decimal

--- javascript.js
+++ javascript.js
@@ -13,234 +13,300 @@ function divide(num1, num2) {
 }
 // Initialise variables
 function display() {
-  let result = ``;
+  let result = `0`;
   let operator = ``;
   let num1 = ``;
   let num2 = ``;
   let operatorCount = 0;
-  const display = document.querySelector(".text");
+  const screen = document.querySelector(".text");
 
-  function clickOne() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `1`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `1`;
-      display.textContent = num2;
-    }
-  }
   const one = document.querySelector(".one");
   one.addEventListener("click", clickOne);
 
-  function clickTwo() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `2`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `2`;
-      display.textContent = num2;
-    }
-  }
   const two = document.querySelector(".two");
   two.addEventListener("click", clickTwo);
 
-  function clickThree() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `3`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `3`;
-      display.textContent = num2;
-    }
-  }
   const three = document.querySelector(".three");
   three.addEventListener("click", clickThree);
 
-  function clickFour() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `4`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `4`;
-      display.textContent = num2;
-    }
-  }
   const four = document.querySelector(".four");
   four.addEventListener("click", clickFour);
 
-  function clickFive() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `5`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `5`;
-      display.textContent = num2;
-    }
-  }
   const five = document.querySelector(".five");
   five.addEventListener("click", clickFive);
 
-  function clickSix() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `6`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `6`;
-      display.textContent = num2;
-    }
-  }
   const six = document.querySelector(".six");
   six.addEventListener("click", clickSix);
 
-  function clickSeven() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `7`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `7`;
-      display.textContent = num2;
-    }
-  }
   const seven = document.querySelector(".seven");
   seven.addEventListener("click", clickSeven);
 
-  function clickEight() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `8`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `8`;
-      display.textContent = num2;
-    }
-  }
   const eight = document.querySelector(".eight");
   eight.addEventListener("click", clickEight);
 
-  function clickNine() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `9`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `9`;
-      display.textContent = num2;
-    }
-  }
   const nine = document.querySelector(".nine");
   nine.addEventListener("click", clickNine);
 
-  function clickZero() {
-    if (operatorCount == 0) {
-      //No operators pressed
-      num2 = ``;
-      num1 = num1 + `0`;
-      display.textContent = num1;
-    } else if (operatorCount) {
-      num2 = num2 + `0`;
-      display.textContent = num2;
-    }
-  }
   const zero = document.querySelector(".zero");
   zero.addEventListener("click", clickZero);
 
   const plus = document.querySelector(".plus");
-  plus.addEventListener("click", () => {
-    if (display.textContent === ``) {
-    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
-      result = operate(num1, num2, operator);
-      num1 = result;
-      num2 = ``;
-      display.textContent = result;
-      operator = `+`;
-      operatorCount = 0;
-    } else {
-      result = display.textContent;
-      result = result + "+";
-      display.textContent = result;
-      operatorCount++;
-      operator = `+`;
-    }
-  });
+  plus.addEventListener("click", clickPlus);
 
   const minus = document.querySelector(".minus");
-  minus.addEventListener("click", () => {
-    if (display.textContent === ``) {
-    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
-      result = operate(num1, num2, operator);
-      num1 = result;
-      num2 = ``;
-      display.textContent = result;
-      operator = `-`;
-      operatorCount = 0;
-    } else {
-      result = display.textContent;
-      result = result + "-";
-      display.textContent = result;
-      operatorCount++;
-      operator = `-`;
-    }
-  });
+  minus.addEventListener("click", clickMinus);
 
   const multiply = document.querySelector(".multiply");
-  multiply.addEventListener("click", () => {
-    if (display.textContent === ``) {
-    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
-      result = operate(num1, num2, operator);
-      num1 = result;
-      num2 = ``;
-      display.textContent = result;
-      operator = `*`;
-      operatorCount = 0;
-    } else {
-      result = display.textContent;
-      result = result + "*";
-      display.textContent = result;
-      operatorCount++;
-      operator = `*`;
-    }
-  });
+  multiply.addEventListener("click", clickMultiply);
 
   const divide = document.querySelector(".divide");
-  divide.addEventListener("click", () => {
-    if (display.textContent === ``) {
+  divide.addEventListener("click", clickDivide);
+
+  const equal = document.querySelector(".equal");
+  equal.addEventListener("click", clickEqual);
+
+  const cancel = document.querySelector(".cancel");
+  cancel.addEventListener("click", clickCancel);
+
+  const backspace = document.querySelector(".backspace");
+  backspace.addEventListener("click", clickBack);
+
+  function clickOne() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `1`;
+        screen.textContent = num2;
+      } else {
+        num1 += `1`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `1`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickTwo() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `2`;
+        screen.textContent = num2;
+      } else {
+        num1 += `2`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `2`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickThree() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `3`;
+        screen.textContent = num2;
+      } else {
+        num1 += `3`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `3`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickFour() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `4`;
+        screen.textContent = num2;
+      } else {
+        num1 += `4`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `4`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickFive() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `5`;
+        screen.textContent = num2;
+      } else {
+        num1 += `5`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `5`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickSix() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `6`;
+        screen.textContent = num2;
+      } else {
+        num1 += `6`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `6`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickSeven() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `7`;
+        screen.textContent = num2;
+      } else {
+        num1 += `7`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `7`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickEight() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `8`;
+        screen.textContent = num2;
+      } else {
+        num1 += `8`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `8`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickNine() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `9`;
+        screen.textContent = num2;
+      } else {
+        num1 += `9`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `9`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickZero() {
+    if (operatorCount === 0) {
+      if (num1 === result) {
+        num2 += `0`;
+        screen.textContent = num2;
+      } else {
+        num1 += `0`;
+        screen.textContent = num1;
+      }
+    } else {
+      screen.textContent = ``;
+      num2 = num2 + `0`;
+      screen.textContent = num2;
+    }
+  }
+
+  function clickPlus() {
+    if (screen.textContent === ``) {
     } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
       result = operate(num1, num2, operator);
       num1 = result;
       num2 = ``;
-      display.textContent = result;
+      screen.textContent = result;
+      operator = `+`;
+      operatorCount = 0;
+    } else {
+      result = screen.textContent;
+      result = result + "+";
+      screen.textContent = result;
+      operatorCount++;
+      operator = `+`;
+    }
+  }
+
+  function clickMinus() {
+    if (screen.textContent === ``) {
+    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+      result = operate(num1, num2, operator);
+      num1 = result;
+      num2 = ``;
+      screen.textContent = result;
+      operator = `-`;
+      operatorCount = 0;
+    } else {
+      result = screen.textContent;
+      result = result + "-";
+      screen.textContent = result;
+      operatorCount++;
+      operator = `-`;
+    }
+  }
+
+  function clickMultiply() {
+    if (screen.textContent === ``) {
+    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+      result = operate(num1, num2, operator);
+      num1 = result;
+      num2 = ``;
+      screen.textContent = result;
+      operator = `*`;
+      operatorCount = 0;
+    } else {
+      result = screen.textContent;
+      result = result + "*";
+      screen.textContent = result;
+      operatorCount++;
+      operator = `*`;
+    }
+  }
+
+  function clickDivide() {
+    if (screen.textContent === ``) {
+    } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+      result = operate(num1, num2, operator);
+      num1 = result;
+      num2 = ``;
+      screen.textContent = result;
       operator = `/`;
       operatorCount = 0;
     } else {
-      result = display.textContent;
+      result = screen.textContent;
       result = result + "/";
-      display.textContent = result;
+      screen.textContent = result;
       operatorCount++;
       operator = `/`;
     }
-  });
+  }
 
-  const equal = document.querySelector(".equal");
-  equal.addEventListener("click", () => {
+  function clickEqual() {
     if (num1 === `` || num2 === ``) {
     } else if (operator === `/` && num2 === `0`) {
-      display.textContent = "bodoh";
+      screen.textContent = "bodoh";
       num1 = ``;
       num2 = ``;
       operatorCount = 0;
@@ -249,45 +315,52 @@ function display() {
       num1 = result;
       num2 = ``;
       operatorCount = 0;
-      display.textContent = num1;
+      screen.textContent = num1;
       console.log(num1);
     }
-  });
+  }
 
-  const cancel = document.querySelector(".cancel");
-  cancel.addEventListener("click", () => {
+  function clickCancel() {
     num1 = ``;
     num2 = ``;
     result = ``;
+    operator = ``;
     operatorCount = 0;
-    display.textContent = ``;
-  });
+    screen.textContent = ``;
+  }
 
-  const backspace = document.querySelector(".backspace");
-  backspace.addEventListener("click", () => {
+  function clickBack() {
     if (num1 === ``) {
     } else {
       if (operatorCount === 0) {
         if (num1.length <= 1) {
           num1 = ``;
-          display.textContent = num1;
+          screen.textContent = num1;
         } else {
+          num1 = String(num1);
           num1 = num1.substring(0, num1.length - 1);
-          display.textContent = num1;
+          if (num1 !== ``) {
+            num1 = parseFloat(num1);
+          }
+          screen.textContent = num1;
         }
       } else {
         if (num2 !== ``) {
           if (num2.length <= 1) {
             num2 = ``;
-            display.textContent = num2;
+            screen.textContent = num2;
           } else {
+            num2 = String(num2);
             num2 = num2.substring(0, num2.length - 1);
-            display.textContent = num2;
+            if (num2 !== ``) {
+              num2 = parseFloat(num2);
+            }
+            screen.textContent = num2;
           }
         }
       }
     }
-  });
+  }
 
   document.addEventListener("keydown", logKey);
   function logKey(e) {
@@ -322,6 +395,27 @@ function display() {
       case `0`:
         clickZero();
         break;
+      case `+`:
+        clickPlus();
+        break;
+      case `-`:
+        clickMinus();
+        break;
+      case `*`:
+        clickMultiply();
+        break;
+      case `/`:
+        clickDivide();
+        break;
+      case `Enter`:
+        clickEqual();
+        break;
+      case `Escape`:
+        clickCancel();
+        break;
+      case `Backspace`:
+        clickBack();
+        break;
       default:
         console.log("other key pressed");
     }
@@ -329,8 +423,8 @@ function display() {
 }
 
 function operate(num1, num2, operator) {
-  num1 = parseInt(num1);
-  num2 = parseInt(num2);
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
   if (operator === `+`) {
     return add(num1, num2);
   } else if (operator === `-`) {
---
+++ lmao.js
@@ -0,0 +1,302 @@
+function clickOne() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `1`;
+      screen.textContent = num2;
+    } else {
+      num1 += `1`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `1`;
+    screen.textContent = num2;
+  }
+}
+
+function clickTwo() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `2`;
+      screen.textContent = num2;
+    } else {
+      num1 += `2`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `2`;
+    screen.textContent = num2;
+  }
+}
+
+function clickThree() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `3`;
+      screen.textContent = num2;
+    } else {
+      num1 += `3`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `3`;
+    screen.textContent = num2;
+  }
+}
+
+function clickFour() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `4`;
+      screen.textContent = num2;
+    } else {
+      num1 += `4`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `4`;
+    screen.textContent = num2;
+  }
+}
+
+function clickFive() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `5`;
+      screen.textContent = num2;
+    } else {
+      num1 += `5`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `5`;
+    screen.textContent = num2;
+  }
+}
+
+function clickSix() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `6`;
+      screen.textContent = num2;
+    } else {
+      num1 += `6`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `6`;
+    screen.textContent = num2;
+  }
+}
+
+function clickSeven() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `7`;
+      screen.textContent = num2;
+    } else {
+      num1 += `7`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `7`;
+    screen.textContent = num2;
+  }
+}
+
+function clickEight() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `8`;
+      screen.textContent = num2;
+    } else {
+      num1 += `8`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `8`;
+    screen.textContent = num2;
+  }
+}
+
+function clickNine() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `9`;
+      screen.textContent = num2;
+    } else {
+      num1 += `9`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `9`;
+    screen.textContent = num2;
+  }
+}
+
+function clickZero() {
+  if (operatorCount === 0) {
+    if (num1 === result) {
+      num2 += `0`;
+      screen.textContent = num2;
+    } else {
+      num1 += `0`;
+      screen.textContent = num1;
+    }
+  } else {
+    num2 = num2 + `0`;
+    screen.textContent = num2;
+  }
+}
+
+function clickPlus() {
+  if (display.textContent === ``) {
+  } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+    result = operate(num1, num2, operator);
+    num1 = result;
+    num2 = ``;
+    display.textContent = result;
+    operator = `+`;
+    operatorCount = 0;
+  } else {
+    result = display.textContent;
+    result = result + "+";
+    display.textContent = result;
+    operatorCount++;
+    operator = `+`;
+  }
+}
+
+function clickMinus() {
+  if (display.textContent === ``) {
+  } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+    result = operate(num1, num2, operator);
+    num1 = result;
+    num2 = ``;
+    display.textContent = result;
+    operator = `-`;
+    operatorCount = 0;
+  } else {
+    result = display.textContent;
+    result = result + "-";
+    display.textContent = result;
+    operatorCount++;
+    operator = `-`;
+  }
+}
+
+function clickMultiply() {
+  if (display.textContent === ``) {
+  } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+    result = operate(num1, num2, operator);
+    num1 = result;
+    num2 = ``;
+    display.textContent = result;
+    operator = `*`;
+    operatorCount = 0;
+  } else {
+    result = display.textContent;
+    result = result + "*";
+    display.textContent = result;
+    operatorCount++;
+    operator = `*`;
+  }
+}
+
+function clickDivide() {
+  if (display.textContent === ``) {
+  } else if (operatorCount == 1 && num1 !== `` && num2 !== ``) {
+    result = operate(num1, num2, operator);
+    num1 = result;
+    num2 = ``;
+    display.textContent = result;
+    operator = `/`;
+    operatorCount = 0;
+  } else {
+    result = display.textContent;
+    result = result + "/";
+    display.textContent = result;
+    operatorCount++;
+    operator = `/`;
+  }
+}
+
+function clickEqual() {
+  if (num1 === `` || num2 === ``) {
+  } else if (operator === `/` && num2 === `0`) {
+    display.textContent = "bodoh";
+    num1 = ``;
+    num2 = ``;
+    operatorCount = 0;
+  } else {
+    result = operate(num1, num2, operator);
+    num1 = result;
+    num2 = ``;
+    operatorCount = 0;
+    display.textContent = num1;
+    console.log(num1);
+  }
+}
+
+function clickCancel() {
+  num1 = ``;
+  num2 = ``;
+  result = ``;
+  operator = ``;
+  operatorCount = 0;
+  display.textContent = ``;
+}
+
+function clickBack() {
+  if (num1 === ``) {
+  } else {
+    if (operatorCount === 0) {
+      if (num1.length <= 1) {
+        num1 = ``;
+        display.textContent = num1;
+      } else {
+        num1 = String(num1);
+        num1 = num1.substring(0, num1.length - 1);
+        if (num1 !== ``) {
+          num1 = parseFloat(num1);
+        }
+        display.textContent = num1;
+      }
+    } else {
+      if (num2 !== ``) {
+        if (num2.length <= 1) {
+          num2 = ``;
+          display.textContent = num2;
+        } else {
+          num2 = String(num2);
+          num2 = num2.substring(0, num2.length - 1);
+          if (num2 !== ``) {
+            num2 = parseFloat(num2);
+          }
+          display.textContent = num2;
+        }
+      }
+    }
+  }
+}
+
+case `+`:
+  clickPlus();
+  break;
+  case `-`:
+  clickMinus();
+  break;
+  case `*`:
+  clickMultiply();
+  break;
+  case `/`:
+  clickDivide();
+  break;
+  case `Enter`:
+  clickEqual();
+  break;
+  case `Escape`:
+  clickCancel();
+  break;
+  case `Backspace`:
+  clickBack();
+  break;
